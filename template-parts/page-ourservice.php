d360992a089863c8045abc5509c147bac10851ff
fitra17 <fitrakun@gmail.com>
2018-08-14 16:20:10 +0700

    Init template parts - our service

---
+++ template-parts/page-ourservice.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying service page format
+ *
+ * Template Name: Our Service Page
+ *
+ * @package WordPress
+ * @subpackage Instrat
+ * @since Instrat 1.0
+ */
+
+get_header(); ?>
+  <div id="primary" class="content-area pagecontainer">
+    <main id="main" class="site-main" role="main" style="margin: 3rem 0 0;">
+      
+    </main><!-- .site-main -->
+  </div> <!-- content area --> 
+<?php get_footer();?>
